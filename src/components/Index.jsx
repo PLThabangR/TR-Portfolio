574d9cec53cd353b45e062f98e270816f6a694fa
PLThabangR <rakgoropthabang@gmail.com>
2024-06-11 18:23:10 +0200

    Added My SQL to the header

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
             <div className="col-xxl-5">
                
                 <div className="text-center text-xxl-start">
-                    <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase">Frontend &middot;Cloud &middot; Python</div></div>
+                    <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase">Frontend &middot;Data Analyst&middot; Python&middot; MySQL</div></div>
                     <div className="fs-3 fw-light text-muted">I can help your business to</div>
                     <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get online and grow fast</span></h1>
                     <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
